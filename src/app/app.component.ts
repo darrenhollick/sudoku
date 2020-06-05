12d6c481d328a9e5e8e505e24f96df58da77e6b8
Darren Hollick <darrenhollick@gmail.com>
2020-06-05 16:16:15 -0400

    fix url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,8 @@ export class AppComponent implements OnInit {
     }
 
     loadPuzzleConfig(puzzleId: string) {
-        console.log(`${window.location.origin}/assets/puzzles/${puzzleId}.json`);
-        this.http.get<PuzzleConfig>(`${window.location.origin}/assets/puzzles/${puzzleId}.json`).subscribe(data => {
+        // console.log(`${window.location.href}/assets/puzzles/${puzzleId}.json`);
+        this.http.get<PuzzleConfig>(`${window.location.href}/assets/puzzles/${puzzleId}.json`).subscribe(data => {
             this.squareConfigs = data.squares;
             this.puzzleInstructions = data.instructions;
             this.puzzleTitle = data.title;
